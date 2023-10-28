9a245b4de0131c9ccc9d9115a1aa9251d5048024
kimyurie <9911dbfl@naver.com>
2023-10-28 15:17:37 +0900

    feat: 셀 클릭 처리 및 돌 색상 변경

--- src/routes/Game2.js
+++ src/routes/Game2.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 function Game2() {
   let [start, setStart] = useState(true);
   let [clickedCells, setClickedCells] = useState([]); // 클릭된 셀의 위치를 배열로 저장
-  let [color, setColor] = useState('');
+  let [color, setColor] = useState(''); // 현재 돌의 색상
 
   var row = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
@@ -20,20 +20,21 @@ function Game2() {
   const handleCellClick = (i, j) => {
     // 클릭된 셀의 위치를 배열에 추가
     setClickedCells([...clickedCells, { i, j }]);
-  };
+    
+    // 현재 클릭된 셀의 돌 색을 저장
+    const newColor = color === 'black' ? 'white' : 'black';
 
-  // 클릭된 셀과 현재 매핑되는 돌의 색상을 결정
-  useEffect(() => {
-    if (clickedCells.length > 0) {
-      // 클릭된 모든 셀에 돌의 색상을 적용
-      setColor('black');
-    }
-  }, [clickedCells]);
+    // 현재 클릭된 셀의 돌 색을 변경
+    document.getElementById(`cell-${i}-${j}`).style.backgroundColor = newColor;
+
+    // 현재 클릭된 돌의 색상을 변경
+    setColor(newColor);
+  };
 
   return (
     <>
       <div>
-        {start == true ? <p>* 흑돌 먼저 시작 !</p> : <p>&nbsp;</p>}
+        {start === true ? <p>* 흑돌 먼저 시작 !</p> : <p>&nbsp;</p>}
         <table className="tb2">
           <tbody>
             {row.map(function (i) {
@@ -42,6 +43,7 @@ function Game2() {
                   {row.map(function (j) {
                     return (
                       <td
+                        id={`cell-${i}-${j}`}
                         className={clickedCells.some(cell => cell.i === i && cell.j === j) ? color : ''}
                         onClick={() => handleCellClick(i, j)}
                       ></td>
