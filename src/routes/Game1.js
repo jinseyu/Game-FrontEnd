33f8336b0563bf40e0271ae48b4b65e2dddb8a12
kimyurie <9911dbfl@naver.com>
2023-12-10 21:19:31 +0900

    post

--- src/routes/Game1.js
+++ src/routes/Game1.js
@@ -3,13 +3,12 @@ import { useEffect, useState } from "react";
 
 // 지뢰찾기
 function Game1() {
-  let col = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  let [col,setCol] = useState(Array.from({length:13},()=>Array.from({length:13},()=>-1)))
   let [count, setCount] = useState(60);
   let [color, setColor] = useState('');
   let [clickedCell, setClickedCell] = useState(new Set());
-
+  // console.log(col)
   const handleCellClick = (i, j) => {
-    setClickedCell([...clickedCell, {i,j}]);
     const newColor = color === '' ? 'white_line' : color;
     setColor(newColor);
 
@@ -20,22 +19,24 @@ function Game1() {
 
     // 눌려있는데 숫자 없으면 0, 안눌려있으면 -1(지뢰 상관없이)
     const encodeGameBoard = () => {
-      return col
-        .map((i) => {
+          console.log()
           return col
-            .map((j) => newClickedCell.has(`${i}-${j}`) ? '0' : '-1')
-            .join("");
-        })
-        .join("/");
-    };
+            .map((a,i)=>{
+              return a.map((b,j)=>{
+                return newClickedCell.has(`${i}-${j}`) ? '0' : '-1'
+              }).join("")
+            }).join("/")
+        }
+      
 
     axios
     .post("https://jsonplaceholder.typicode.com/posts", {
-      color : newColor === 'white_line' ? '0' : '-1',
-      location: (13 - i) + "," + (j-1),
+      // location: (13 - i) + "," + (j-1),
+      location: (i) + "," + (j),
       situation : encodeGameBoard(),
     })
     .then((res) => {
+      setCol(res.data.situation.split("/").reverse().map(e=>e.split("")))
       console.log(res.data);
     })
     .catch((err) => {
@@ -43,18 +44,18 @@ function Game1() {
     }); 
   }
 
-  useEffect(() => {
-    let a = setInterval(function () {
-      count -= 1;
-      if (count >= 0) {
-        setCount(count);
-      }
-    }, 1000);
+  // useEffect(() => {
+  //   let a = setInterval(function () {
+  //     count -= 1;
+  //     if (count >= 0) {
+  //       setCount(count);
+  //     }
+  //   }, 1000);
   
-    return () => {
-      clearTimeout(a);
-    };
-  }, []);
+  //   return () => {
+  //     clearTimeout(a);
+  //   };
+  // }, []);
 
   return (
     <div>
@@ -62,19 +63,19 @@ function Game1() {
 
       <table className="tb1">
         <tbody>
-        {col.map((i) => {
+        {col.map((_,i) => {
             return (
               <tr>
-                {col.map((j) => {
+                {col.map((_,j) => {
                   return (
                     <td
                       // 클릭한 칸이 흑 -> 백으로 바뀌게
                       id={`cell-${i}-${j}`}
-                      className={clickedCell.has(`${i}-${j}`)? color : ''}
-                      onClick={() => {handleCellClick(i, j)}} 
+                      className={clickedCell.has(`${12-i}-${j}`)? color : ''}
+                      onClick={() => {handleCellClick(12-i, j)}} 
                     >
-                        {/* 흰색 칸 안에 숫자 1 표시 */}
-                  {clickedCell.has(`${i}-${j}`) && color === 'white_line' && (
+                  {/* 흰색 칸 안에 숫자 1 표시  */}
+                  {clickedCell.has(`${12-i}-${j}`) && color === 'white_line' && (
                     <div className="number">1</div>
                   )}
                     </td>
@@ -90,3 +91,4 @@ function Game1() {
 }
 
 export default Game1;
+
